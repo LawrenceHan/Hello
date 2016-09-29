b8144b857dac5ee878b9107fcfc221444a6f8027
LawrenceHan <yangfei6565@163.com>
2016-09-29 15:49:00 +0800

    Added test code.

--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -1,13 +1,43 @@
 import Vapor
+import HTTP
 
 let drop = Droplet()
 var path = ""
 
+// Home page
 drop.get { req in
     let lang = req.headers["Accept-Language"]?.string ?? "en"
     return try drop.view.make("welcome", [
     	"message": Node.string(drop.localization[lang, "welcome", "title"])
     ])
+}
+
+// Redirect
+drop.get("vapor") { request in
+    return Response(redirect: "http://vapor.codes")
+}
+
+// Json return
+drop.get("json") { request in
+    return try JSON(node: [
+        "number": 123,
+        "text": "unicorns",
+        "bool": false
+        ])
+}
+
+// Error throwing
+drop.get("error") { (req) -> ResponseRepresentable in
+    throw Abort.custom(status: .badRequest, message: "Sorry, this is a test")
+}
+
+// Fallback test
+drop.get("*") { (req) -> ResponseRepresentable in
+    var path = req.uri.path
+    if path.contains("/") {
+        path.remove(at: path.startIndex)
+    }
+    return "Hello \(path)"
 }
 
 drop.resource("posts", PostController())
