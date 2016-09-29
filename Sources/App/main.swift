c361c7e1c8b15e6230c7a755a250719c307ef2dd
LawrenceHan <yangfei6565@163.com>
2016-09-29 15:20:43 +0800

    Added inital codes.

---
+++ Sources/App/main.swift
@@ -0,0 +1,15 @@
+import Vapor
+
+let drop = Droplet()
+var path = ""
+
+drop.get { req in
+    let lang = req.headers["Accept-Language"]?.string ?? "en"
+    return try drop.view.make("welcome", [
+    	"message": Node.string(drop.localization[lang, "welcome", "title"])
+    ])
+}
+
+drop.resource("posts", PostController())
+
+drop.run()
